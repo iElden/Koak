1a25feb8ff46392b4437800e6b049b30817388ff
Jonathan Martin <jonathan.martin@epitech.eu>
2020-02-28 15:49:10 +0100

    fixed compilation error

--- src/LLVMConverter.hs
+++ src/LLVMConverter.hs
@@ -8,11 +8,12 @@ import Data.Text.Lazy.IO as T
 
 import LLVM.Pretty  -- from the llvm-hs-pretty package
 import LLVM.AST hiding (function)
-import LLVM.AST.Type as AST
+import LLVM.AST.Type as ASTL
 import qualified LLVM.AST.Float as F
 import qualified LLVM.AST.Constant as C
 import qualified LLVM.AST.Operand as O
 import qualified LLVM.AST.IntegerPredicate as I
+import qualified LLVM.AST.ParameterAttribute as PA
 
 import Data.String
 import LLVM.IRBuilder.Module
@@ -24,19 +25,34 @@ import qualified LLVM.Context as Ctx
 import qualified LLVM.Module as Module
 import qualified LLVM.Target as Target
 
+floatType = FloatingPointType DoubleFP
+
+getParamsValueInLLVM :: MonadModuleBuilder m => [Value] -> IRBuilderT m [(Operand, [PA.ParameterAttribute])]
+getParamsValueInLLVM list = sequence $ fmap (\s -> fmap (\f -> (f, [])) s) $ fmap convertValue list
+--getParamsValueInLLVM [] = return []
+--getParamsValueInLLVM (c:cs) = sequence ((convertValue c), []) ++ getParamsValueInLLVM cs
+
+getParamsTypeInLLVM :: [(String, AST.Type)] -> [ASTL.Type]
+getParamsTypeInLLVM [] = []
+getParamsTypeInLLVM ((_, FloatingVar):cs) = [floatType] ++ getParamsTypeInLLVM cs
+getParamsTypeInLLVM ((_, IntegerVar):cs) = [floatType] ++ getParamsTypeInLLVM cs
+getParamsTypeInLLVM (_:cs) = [floatType] ++ getParamsTypeInLLVM cs
+
 convertVariable :: MonadModuleBuilder m => Value -> Expression -> IRBuilderT m Operand
 convertVariable (Var n _) expr = do
     res <- convertExpression expr
     case res of
-        (ConstantOperand cons) -> global (fromString n) (FloatingPointType DoubleFP) cons
-        _ -> global (fromString n) (FloatingPointType DoubleFP) $ (C.Float $ F.Double 0.0)
-convertVariable _ expr = global (fromString n) (FloatingPointType DoubleFP) $ (C.Float $ F.Double 0.0)
+        (ConstantOperand cons) -> global (fromString n) floatType cons
+        _ -> global (fromString n) floatType $ (C.Float $ F.Double 0.0)
 
 convertValue :: MonadModuleBuilder m => Value -> IRBuilderT m Operand
 convertValue (Nbr n) = CB.double $ fromIntegral n
 convertValue (RealNbr n) = CB.double n
-convertValue (Var n FloatingVar) = return $ ConstantOperand $ C.GlobalReference (FloatingPointType DoubleFP) (fromString n)
-convertValue (Var n IntegerVar) = return $ ConstantOperand $ C.GlobalReference (FloatingPointType DoubleFP) (fromString n)
+convertValue (Var n FloatingVar) = return $ ConstantOperand $ C.GlobalReference floatType (fromString n)
+convertValue (Var n IntegerVar) = return $ ConstantOperand $ C.GlobalReference floatType (fromString n)
+convertValue (AST.Call (Proto name params retType) args) = do
+    parameters <- getParamsValueInLLVM args
+    call (ConstantOperand $ C.GlobalReference (FunctionType floatType (getParamsTypeInLLVM params) False) $ fromString name) parameters
 --convertValue (GlobVar n) = do
 --    return $ LocalReference (FloatingPointType DoubleFP) $ fromString n
 --convertValue (Var n IntegerVar) = LocalReference (IntegerType 32) $ fromString n
@@ -65,10 +81,10 @@ convertExpression (Expr (Unary [] val) AST.Asg expr) = do
 
 makeASTModule :: String -> [Expression] -> Module
 makeASTModule name [] = buildModule (fromString name) $ do
-  function "main" [] i32 $ \_ -> do
+  function "main" [] double $ \_ -> do
     value <- CB.int32 $ 0
     ret $ value
 makeASTModule name exprs = buildModule (fromString name) $ do
-  function "main" [] i32 $ \_ -> do
+  function "main" [] double $ \_ -> do
     operands <- traverse convertExpression exprs
     ret $ last operands
